0a13a6bf6bfa6f9ece0cca66a29d4d67c5db11c6
Chris Toppan <chris@toppan.org>
2026-04-05 16:23:04 -0500

    Update initHacking.js

--- src/bootstrap/initHacking.js
+++ src/bootstrap/initHacking.js
@@ -5,6 +5,7 @@ export async function main(ns) {
     const killAllScript = "/hacking/main/killAll.js";
     const xpDistributor = "/xp/xpDistributor.js";
     const gangManager = "/gang/gangManager_v2.js";
+    const crimeManager = "/crime/crimeManager.js";
 
     if (!ns.fileExists(killAllScript, "home")) {
         ns.tprint(`[${ts()}] ERROR: Missing ${killAllScript}`);
@@ -31,15 +32,36 @@ export async function main(ns) {
         ns.tprint(`[${ts()}] ERROR: Failed to start ${xpDistributor}`);
     }
 
-    if (ns.fileExists(gangManager, "home")) {
-        await ns.sleep(250);
-        ns.tprint(`[${ts()}] Starting ${gangManager} 150e9 money rep`);
-        const gangPid = ns.run(gangManager, 1, 150e9, "money", "rep");
-        if (gangPid === 0) {
-            ns.tprint(`[${ts()}] WARNING: Failed to start ${gangManager}`);
+    await ns.sleep(250);
+
+    if (hasGang(ns)) {
+        if (ns.fileExists(gangManager, "home")) {
+            ns.tprint(`[${ts()}] Gang unlocked, starting ${gangManager} 150e9 money rep`);
+            const gangPid = ns.run(gangManager, 1, 150e9, "money", "rep");
+            if (gangPid === 0) {
+                ns.tprint(`[${ts()}] WARNING: Failed to start ${gangManager}`);
+            }
+        } else {
+            ns.tprint(`[${ts()}] Gang unlocked, but missing ${gangManager}`);
         }
     } else {
-        ns.tprint(`[${ts()}] Skipping gang manager, file missing`);
+        if (ns.fileExists(crimeManager, "home")) {
+            ns.tprint(`[${ts()}] Gang not unlocked, starting ${crimeManager}`);
+            const crimePid = ns.run(crimeManager, 1);
+            if (crimePid === 0) {
+                ns.tprint(`[${ts()}] WARNING: Failed to start ${crimeManager}`);
+            }
+        } else {
+            ns.tprint(`[${ts()}] Gang not unlocked, and missing ${crimeManager}`);
+        }
+    }
+}
+
+function hasGang(ns) {
+    try {
+        return !!ns.gang && ns.gang.inGang();
+    } catch {
+        return false;
     }
 }
 
